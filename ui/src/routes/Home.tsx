40ecbddc6a1e61a3c45971c96245e9fc7325748a
nkpe <naomi.pearse@gmail.com>
2024-08-21 16:11:50 +0100

    front-end added

---
+++ ui/src/routes/Home.tsx
@@ -0,0 +1,20 @@
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Home = () => {
+    return (
+        <main id="homepage">
+            <h2>Welcome to Recipe Finder</h2>
+            <form >
+                <label htmlFor="ingredient-search">Search by ingredient</label>
+                <select id="ingredient-search" name="ingredient-search">
+                    <option value="chicken">Chicken</option>
+                    <option value="pork">Pork</option>
+                    <option value="cheese">Cheese</option>
+                </select>                
+                <button typeof="submit">Search</button>
+            </form>
+        </main>
+    )
+};
+
+export default Home;
