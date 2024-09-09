c8287f39b25c6253b8e303c219c78bd7b11841b8
nkpe <naomi.pearse@gmail.com>
2024-09-09 14:08:45 +0100

    Homepage layout centered using Box instead of Grid

--- ui/src/routes/Home.tsx
+++ ui/src/routes/Home.tsx
@@ -1,30 +1,19 @@
-import { Typography } from '@mui/material';
-import Grid from '@mui/material/Grid2';
+import { Box, Typography } from '@mui/material';
 
 const Home = () => {
     return (
-        <Grid container spacing={10} direction='column' id="homepage">
-            <Grid display="flex" justifyContent="center" alignItems="center" size="grow">
-                <Typography variant="h3" component="h1">Welcome to Recipe Finder</Typography>
-            </Grid>
-            <Grid display="flex" justifyContent="center" alignItems="center" size="grow" rowGap={3}>
-                <form>
-                    <Grid display="flex" justifyContent="center" alignItems="center" size="grow">
-                        <label htmlFor="ingredient-search">Search by ingredient</label>
-                    </Grid>
-                    <Grid display="flex" justifyContent="center" alignItems="center" size="grow">
-                        <select id="ingredient-search" name="ingredient-search">
-                            <option value="chicken">Chicken</option>
-                            <option value="pork">Pork</option>
-                            <option value="cheese">Cheese</option>
-                        </select>
-                    </Grid>
-                    <Grid display="flex" justifyContent="center" alignItems="center" size="grow">
-                        <button typeof="submit">Search</button>
-                    </Grid>
-                </form>
-            </Grid>
-        </Grid>
+        <Box id="homepage-container" className="flexBoxColumn">
+            <Typography variant="h3" component="h1">Welcome to Recipe Finder</Typography>
+            <form className="flexBoxColumn">
+            <Typography variant="h5" component="h1"><label htmlFor="ingredient-search">Search by ingredient</label></Typography>
+                <select id="ingredient-search" name="ingredient-search">
+                    <option value="chicken">Chicken</option>
+                    <option value="pork">Pork</option>
+                    <option value="cheese">Cheese</option>
+                </select>
+                <button typeof="submit">Search</button>
+            </form>
+        </Box>
     )
 };
 
