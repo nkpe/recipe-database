5582c3b11654bf2617165a210f0c2d137a40c22b
nkpe <naomi.pearse@gmail.com>
2024-09-19 15:34:27 +0100

    mui components replace select and form components

--- ui/src/routes/Home.tsx
+++ ui/src/routes/Home.tsx
@@ -1,18 +1,36 @@
-import { Box, Typography } from '@mui/material';
+import { Box, InputLabel, FormControl, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import { useState } from 'react';
 
 const Home = () => {
+    const [ingredientItem, setIngredientItem] = useState("")
+    const handleChange = (event: SelectChangeEvent) => {
+        setIngredientItem(event.target.value);
+    }
     return (
-        <Box id="homepage-container" className="flexBoxColumn">
+        <Box id="homepage-container" className="flexBoxColumn" style={{backgroundColor: 'pink'}}>
             <Typography variant="h3" component="h1">Welcome to Recipe Finder</Typography>
-            <form className="flexBoxColumn">
-            <Typography variant="h5" component="h1"><label htmlFor="ingredient-search">Search by ingredient</label></Typography>
-                <select id="ingredient-search" name="ingredient-search">
-                    <option value="chicken">Chicken</option>
-                    <option value="pork">Pork</option>
-                    <option value="cheese">Cheese</option>
-                </select>
-                <button typeof="submit">Search</button>
-            </form>
+            <Box id="homepage-container" className="flexBoxColumn">
+                <FormControl sx={{ m: 1, minWidth: 500 }} size="medium">
+                <InputLabel 
+                    id="ingredient-search-select"
+                >
+                    Search by ingredient</InputLabel>
+                    <Select 
+                        id="ingredient-search" 
+                        labelId="ingredient-search-select" 
+                        name="ingredient-search"
+                        label="Ingredients"
+                        value={ingredientItem}
+                        onChange={handleChange}
+                        variant="standard"
+                        style={{backgroundColor: 'turquoise'}}>
+                        <MenuItem value="chicken">Chicken</MenuItem>
+                        <MenuItem value="pork">Pork</MenuItem>
+                        <MenuItem value="cheese">Cheese</MenuItem>
+                    </Select>
+                    <button typeof="submit">Search</button>
+                </FormControl>
+            </Box>
         </Box>
     )
 };
