2f39d87d05985b6e95a80cbebaaed72655abd889
nkpe <naomi.pearse@gmail.com>
2024-10-17 13:20:26 +0100

    Vite added and styling and html pulled out

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,52 +1,13 @@
-import { Box, CssBaseline, Stack, ThemeProvider, alpha } from '@mui/material';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import { theme } from './theme';
-import Home from "./routes/Home";
-import Header from "./components/static/Header";
-import Footer from './components/static/Footer';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    // loader: rootLoader,
-    // children: [
-    //   {
-    //     path: "recipes",
-    //     element: <Recipes />,
-    //     // loader: recipeLoader,
-    //   }
-    // ],
-  },
-]);
-
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <Box className="app-box" sx={(theme) => ({
-          position: { sm: 'relative', md: '' },
-          minHeight: "100vh",
-          flexGrow: 1,
-          backgroundColor: alpha(theme.palette.background.default, 1),
-          overflow: 'auto',
-        })} >
-          <Stack direction="column" sx={{ height: "100%" }}>
-            <Header />
-            <Box component="main" sx={(theme) =>({
-              minHeight: "calc(100vh + 64px)",
-            })}>
-              <RouterProvider router={router} />
-            </Box>
-            <Footer />
-          </Stack>
-        </Box>
-      </CssBaseline>
-    </ThemeProvider>
+    <main>
+      Hello
+    </main>
   )
 }
 
-export default App;
+export default App
