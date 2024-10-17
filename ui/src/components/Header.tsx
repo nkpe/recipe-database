b5475f381e898fa0065382f87f54deb0999dbc0f
nkpe <naomi.pearse@gmail.com>
2024-10-17 16:10:39 +0100

    Header styling added via classes

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header className="sticky top-0 z-50 w-full flex bg-primary h-12 justify-center items-center">
+        <header className="sticky top-0 z-50 w-full flex bg-primary h-12 justify-center items-center text-background">
             <p className="text-3xl">Recipe<b>Finder</b></p>
         </header>
     )
