6a82057c5f41c28cd1fcfed99d51f254a2081a9f
nkpe <naomi.pearse@gmail.com>
2024-10-21 09:14:49 +0100

    categories moved inside component

--- ui/src/components/Home/CategoryIcons.tsx
+++ ui/src/components/Home/CategoryIcons.tsx
@@ -9,15 +9,15 @@ import {
 } from "@/components/ui/tooltip"
 
 
-const categories = [
-    { category: "Soups and Starters", value: "starter", icon: <Soup /> },
-    { category: "Meat", value: "meat", icon: <Ham /> },
-    { category: "Fish", value: "fish", icon: <FishSymbol /> },
-    { category: "Veggie", value: "veggie", icon: <LeafyGreen /> },
-    { category: "Desserts", value: "dessert", icon: <CakeSlice /> },
-]
-
 export const CategoryIcons = () => {
+    const categories = [
+        { category: "Soups and Starters", value: "starter", icon: <Soup /> },
+        { category: "Meat", value: "meat", icon: <Ham /> },
+        { category: "Fish", value: "fish", icon: <FishSymbol /> },
+        { category: "Veggie", value: "veggie", icon: <LeafyGreen /> },
+        { category: "Desserts", value: "dessert", icon: <CakeSlice /> },
+    ]
+
     return (
         <div className="category-container flex items-center flex-col gap-2">
             <Label htmlFor="category-select">Categories</Label>
