e6d1d28eab743b21d17ad476697d23b0125f6984
nkpe <naomi.pearse@gmail.com>
2024-11-04 14:50:50 +0000

    tailwind theme amended with main and heading styling

--- ui/src/components/Home/Home.tsx
+++ ui/src/components/Home/Home.tsx
@@ -6,7 +6,7 @@ import { FeaturedRecipes } from "./FeaturedRecipes";
 
 export const Home = ({recipes}: {recipes: Recipe[]}) => {
     return (
-        <main className="h-screen flex m-4 box-border gap-4">
+        <main className="flex">
             <Aside recipes={recipes} allergenCategories={allergenCategory}/>
             <div className="h-100 flex-col w-4/5">
                 <FeaturedRecipes recipes={recipes} />
--- ui/src/components/RecipePage/RecipePage.tsx
+++ ui/src/components/RecipePage/RecipePage.tsx
@@ -6,19 +6,21 @@ import { Recipe } from "@/lib/recipeData";
 export const RecipePage = ({ recipe }: { recipe: Recipe }) => {
     return (
         <main>
-            <div className="">
-                <h1>{recipe.name}</h1>
+            <div className="recipe-top flex flex-row">
                 <div className="w-1/4 aspect-square overflow-hidden ">
                     <img className="h-full w-full object-cover" src={recipe.img} alt={recipe.name} />
                 </div>
+                <h1>{recipe.name}</h1>
             </div>
             <Separator />
-            <ul>
-                {recipe.ingredients.map((ingredient, i) => {
-                    return <li key={i}>{ingredient}</li>
-                })}
-            </ul>
-            <article>{recipe.instructions}</article>
+            <div className="recipe-details">
+                <ul>
+                    {recipe.ingredients.map((ingredient, i) => {
+                        return <li key={i}>{ingredient}</li>
+                    })}
+                </ul>
+                <article>{recipe.instructions}</article>
+            </div>
         </main>
     )
 }
