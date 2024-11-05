22621b6e3be7dd452977be7371b3a3a5e4ca5c33
nkpe <naomi.pearse@gmail.com>
2024-11-05 09:46:31 +0000

    removed allergen icons from AllergySelect and added to Recipe Page

--- ui/src/components/Home/AllergySelect.tsx
+++ ui/src/components/Home/AllergySelect.tsx
@@ -1,16 +1,7 @@
-// import { BeanOff, EggOff, MilkOff, NutOff, WheatOff } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label"
 
 export const AllergySelect = ({ allergenCategories }: { allergenCategories: string[] }) => {
-
-    // const allergensIcons = [
-    //     { allergen: "gluten", value: "gluten", icon: <WheatOff /> },
-    //     { allergen: "dairy", value: "dairy", icon: <MilkOff /> },
-    //     { allergen: "nuts", value: "nuts", icon: <NutOff /> },
-    //     { allergen: "eggs", value: "eggs", icon: <EggOff /> },
-    //     { allergen: "soy", value: "soy", icon: <BeanOff /> }
-    // ]
 
     return (
         <div className="allergy-container flex flex-col gap-2" id="allergy-container">
--- ui/src/components/RecipePage/RecipePage.tsx
+++ ui/src/components/RecipePage/RecipePage.tsx
@@ -1,23 +1,47 @@
+import { Bean, Egg, Milk, Nut, Wheat } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 import { Recipe } from "@/lib/recipeData";
 
+const allergenIcons = [
+    { allergen: "gluten", value: "gluten", icon: <Wheat /> },
+    { allergen: "dairy", value: "dairy", icon: <Milk /> },
+    { allergen: "nuts", value: "nuts", icon: <Nut /> },
+    { allergen: "eggs", value: "eggs", icon: <Egg /> },
+    { allergen: "soy", value: "soy", icon: <Bean /> }
+]
+
+// ADD ALLERGENS WITH ICONS 
+// ADD MEAL CATEGORIES USING SHADCN BADGE
 
 export const RecipePage = ({ recipe }: { recipe: Recipe }) => {
     return (
         <main>
-            <div className="recipe-top flex flex-row">
+            <div className="recipe-top-wrapper flex flex-row">
                 <div className="w-1/4 aspect-square overflow-hidden ">
                     <img className="h-full w-full object-cover" src={recipe.img} alt={recipe.name} />
                 </div>
-                <div className="recipe-info-wrapper">
+
+                <div id="recipe-info-wrapper" className="flex flex-col gap-4">
                     <h1>{recipe.name}</h1>
                     <p>{recipe.description}</p>
-                    <div className="recipe-timings">
-                        <ul className="flex gap-4">
-                            <li>Prep time: {recipe.prepTime}</li>
-                            <li>Total time: {recipe.totalTime}</li>
-                        </ul>
+                    <ul className="flex gap-4 flex-row ">
+                        <li>Prep time: {recipe.prepTime}</li>
+                        <li>Total time: {recipe.totalTime}</li>
+                    </ul>
+
+                    <div id="recipe-details flex flex-col gap-4">
+                        <div id="recipe-allergens">
+                            <h2>Allergens:</h2>
+                            <ul className="flex flex-row flex-wrap gap-2">
+                                {recipe.allergenCategories.map((al: string, i: number) => {
+                                    const icon = allergenIcons.find(({ allergen }) => allergen === al);
+                                    console.log("icon: ", icon)
+                                    return <li id={`${icon?.allergen}-category`} key={i} className="flex flex-row">{icon?.icon} <p>{icon?.allergen}</p></li>
+                                })}
+                            </ul>
+                        </div>
+                        <div id="recipe-categories"></div>
                     </div>
                 </div>
             </div>
