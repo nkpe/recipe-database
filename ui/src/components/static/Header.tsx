8f357c4eba1d817c8fcec005fffe9b9e7c0cf72f
nkpe <naomi.pearse@gmail.com>
2024-11-18 14:07:35 +0000

    Turned RecipeFinder name in header, into a link to homepage

--- ui/src/components/static/Header.tsx
+++ ui/src/components/static/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="sticky top-0 z-50 w-full flex bg-primary h-12 justify-center items-center text-background">
-            <p className="text-3xl">Recipe<b>Finder</b></p>
+            <p className="text-3xl"><a href="/">Recipe<b>Finder</b></a></p>
         </header>
     )
 }
