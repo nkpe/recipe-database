8766cb5072aa0b61e264021539d78bde4ba86840
nkpe <naomi.pearse@gmail.com>
2024-11-18 16:52:56 +0000

    Font weight and colour changed in theme. Recipe Page prep and total times now show in hours and minutes

--- ui/src/components/RecipePage/RecipePage.tsx
+++ ui/src/components/RecipePage/RecipePage.tsx
@@ -12,7 +12,26 @@ const allergenIcons = [
     { allergen: "soy", value: "soy", icon: <Bean /> }
 ]
 
-const RecipeIngredientList = ({ recipe }: {recipe: Recipe}) => {
+const RecipeTimeInfo = ({ recipe }: { recipe: Recipe }) => {
+    const toHoursAndMinutes = (totalMins: number) => {
+        const hours: number  = Math.floor(totalMins / 60);
+        const mins: number  = totalMins % 60;
+
+        return `${hours > 0 ? `${hours} hours`: ''} ${mins > 0 ? ` ${mins} minutes`: ''}`;
+    }
+    
+    const prepTimeMins: string = toHoursAndMinutes(recipe.prepTime);
+    const totalTimeMins: string = toHoursAndMinutes(recipe.totalTime);
+
+    return (
+        <ul className="flex gap-4 flex-row ">
+            <li>Prep time: {prepTimeMins} </li>
+            <li>Total time: {totalTimeMins} </li>
+        </ul>
+    )
+};
+
+const RecipeIngredientList = ({ recipe }: { recipe: Recipe }) => {
     return (
         <div className="flex flex-col gap-2">
             <h2>Ingredients</h2>
@@ -23,39 +42,41 @@ const RecipeIngredientList = ({ recipe }: {recipe: Recipe}) => {
             </ul>
         </div>
     )
-}
+};
 
-const RecipeInstructions = ({recipe}: {recipe: Recipe}) =>{
+const RecipeInstructions = ({ recipe }: { recipe: Recipe }) => {
     return (
         <div className="flex flex-col gap-2">
             <h2>Step by Step instructions</h2>
             <article>
                 <ul>
-                {recipe.instructions.map((step, i) => {
-                    return <li key={i}>{step}</li>
-                })}
+                    {recipe.instructions.map((step, i) => {
+                        return <li key={i}>{step}</li>
+                    })}
                 </ul>
             </article>
         </div>
     )
-}
+};
 
 export const RecipePage = ({ recipe }: { recipe: Recipe }) => {
     console.log("CAT: ", recipe.mealCategories)
     return (
         <main className="flex flex-col gap-4">
-            <div className="recipe-top-wrapper flex flex-row gap-4">
+            <div id="recipe-header" className="flex flex-col gap-4">
+                <h1>{recipe.name}</h1>
+                <RecipeTimeInfo recipe={recipe} />
+            </div>
+            <Separator />
+            <div className="recipe-top-wrapper flex flex-row gap-4 py-3">
                 <div className="w-1/4 aspect-square overflow-hidden ">
                     <img className="h-full w-full object-cover" src={recipe.img} alt={recipe.name} />
                 </div>
 
                 <div id="recipe-info-wrapper" className="flex flex-col gap-4">
-                    <h1>{recipe.name}</h1>
+
                     <p>{recipe.description}</p>
-                    <ul className="flex gap-4 flex-row ">
-                        <li>Prep time: {recipe.prepTime}</li>
-                        <li>Total time: {recipe.totalTime}</li>
-                    </ul>
+                    
 
                     <div id="recipe-categories" className="flex flex-col gap-4">
                         <div id="recipe-allergens" className="flex flex-col gap-2">
@@ -70,7 +91,7 @@ export const RecipePage = ({ recipe }: { recipe: Recipe }) => {
 
                         <div id="recipe-meal-categories" className="flex flex-col gap-2">
                             <h2>Categories</h2>
-                            <ul className="flex flex-row flex-wrap gap-2"> 
+                            <ul className="flex flex-row flex-wrap gap-2">
                                 {recipe.mealCategories.map((cat: string, i: number) => {
                                     console.log("cat: ", cat);
                                     return <li key={i}><Badge variant="secondary" >{cat}</Badge></li>
@@ -82,9 +103,9 @@ export const RecipePage = ({ recipe }: { recipe: Recipe }) => {
             </div>
             <Separator />
             <div className="recipe-details flex flex-col gap-4">
-                <RecipeIngredientList recipe={recipe}/>
+                <RecipeIngredientList recipe={recipe} />
                 <RecipeInstructions recipe={recipe} />
             </div>
         </main>
     )
-}
+};
