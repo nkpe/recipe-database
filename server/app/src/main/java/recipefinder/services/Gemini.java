50bfcc57a109197fd81ac5af6a804d702cd25108
nkpe <naomi.pearse@gmail.com>
2024-12-20 15:43:44 +0000

    Gemini working properly

--- server/app/src/main/java/recipefinder/services/Gemini.java
+++ server/app/src/main/java/recipefinder/services/Gemini.java
@@ -1,7 +1,7 @@
 package recipefinder.services;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import recipefinder.services.GeminiResult;
+// import recipefinder.services.GeminiResult;
 public class Gemini {
     public GeminiResult recipeAIHelper (String prompt) {
         boolean statusOk = true;
@@ -15,10 +15,11 @@ public class Gemini {
             
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
-            
+
             while ((line = reader.readLine()) != null) {
                 System.out.println("Output form Python Script: " + line);
-                result = line;
+                // !!!! reader.readLined() return multiple strings that are needed. Result needs to append these all to a string or list to keep them. 
+                result = result.concat(" " +  line);
             }
             
             BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
--- server/python-gemini/gemini.py
+++ server/python-gemini/gemini.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv
-import google.generativeai as genai;
+import google.generativeai as genai
+import sys
 
 load_dotenv()
 
+def gemini():
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+    genai.configure(api_key=gemini_api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    argument = sys.argv[1]
+    print("PYTHON ARGUMENT: ", argument)
+    response = model.generate_content(sys.argv[1])
+    print("PYTHON RESPONSE: ",response.text)
+    return response.text
 
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
+gemini()
